1b9c59f721ca44ed872c44fd6eaa36f4e19789d4
LizvethM <morales-garciaLizveth2021@email.com>
2020-07-16 15:41:24 +0000

    Pseudocode_Lizveth

---
+++ main.py
@@ -0,0 +1,31 @@
+#    *
+#   * *
+#  *   *
+# *     *
+#*       *
+
+# take userinput diamond
+userinput = input(str("Give me a character to turn into a diamond: "))
+#diamond buliding parts
+d1=     userinput
+d2=   userinput+ userinput
+d3=  userinput+   userinput
+d4= userinput+     userinput
+d5=userinput+       userinput
+
+#printdiamond 
+print(d1+""+
+""+d2+
+""+d3+
+""+d4+
+""+d5+
+""+d4+
+""+d3+
+""+d2+
+""+d1)
+
+
+
+
+#print 1,2,3,4,5,4,3,2,1
+
